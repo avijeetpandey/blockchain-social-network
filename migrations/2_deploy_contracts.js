3bce456743e7c76f5fc1013c3f6577404d83cbbd
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-27 09:44:22 +0530

    initial Setup and migrations

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const SocialNetwork = artifacts.require("SocialNetwork");
+
+module.exports = function(deployer) {
+    deployer.deploy(SocialNetwork);
+};
+
+
---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract SocialNetwork{
+    string public name;
+    constructor() public{
+        name="BlockChain Social Network";
+    }
+}
