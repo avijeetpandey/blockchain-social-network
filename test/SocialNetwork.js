b7782e9573c7505b539677f159bab14d61ac5bce
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-27 10:29:30 +0530

    added createPost tests

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -5,7 +5,7 @@ require('chai').
     use(require('chai-as-promised')).
     should();
 
-contract('SocialNetwork',(accounts)=>{
+contract('SocialNetwork',([deployer , author , tipper])=>{
     let socialNetwork;
 
     before(async ()=>{
@@ -25,7 +25,43 @@ contract('SocialNetwork',(accounts)=>{
             const name = await socialNetwork.name();
             assert.equal(name,'BlockChain Social Network');
         });
-    })
+    });
+
+    describe('posts',async ()=>{
+        let result , postCount;
+        // test for create posts
+        it('creates post',async()=>{
+            result =await socialNetwork.createPost("This is my first post",{
+                from :author
+            });
+            postCount = await socialNetwork.postCount();
+
+            //success case
+            assert.equal(postCount,1);
+            const event=result.logs[0].args;
+            assert.equal(event.id.toNumber(),postCount.toNumber(),'id is correct');
+            assert.equal(event.content,"This is my first post",'content is correct');
+            assert.equal(event.tipAmount,0,'tip amount is correct');
+            assert.equal(event.author,author,'author is correct');
+
+            //case of failure
+            await socialNetwork.createPost("",{
+                from :author
+            }).should.be.rejected;
+        });
+
+        // test for lists posts
+        it('list posts',async()=>{
+
+        });
+
+        //test for post payment
+        it('allows user to tip posts',async()=>{
+
+        });
+
+    });
+
 });
 
 
