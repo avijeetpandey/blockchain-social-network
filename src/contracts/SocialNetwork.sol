c02a69063242a9eb014fe2ed1a42c979b2e1805b
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-27 11:16:29 +0530

    added eth transfer to author

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -16,14 +16,21 @@ contract SocialNetwork{
         uint id; //unsigned integer
         string content;
         uint tipAmount;
-        address author; //hex address
+        address payable author; //hex address
     }
 
     event PostCreated(
         uint id,
         string content,
         uint tipAmount,
-        address author
+        address payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
     );
 
     // function to create posts
@@ -35,6 +42,20 @@ contract SocialNetwork{
         posts[postCount]=  Post(postCount,_content,0,msg.sender);   // mapping to the posts map declared above
         // Events
         emit PostCreated(postCount,_content,0,msg.sender);
+    }
+
+    // function for tip posts
+    function tipPost(uint _id) public payable{
+        require(_id >0 && _id==postCount);
+       // FETCH the post
+        Post memory _post=posts[_id];
+        address payable _author =  _post.author;
+        address(_author).transfer(msg.value);
+        // paying the author ethereum coins
+        _post.tipAmount = _post.tipAmount + msg.value;
+        posts[_id]=_post;
+        // trigger the event
+        emit PostTipped(postCount,_post.content,_post.tipAmount,_author);
     }
 
 }
