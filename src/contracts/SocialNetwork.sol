1a11a3ec706e715335b8e29a3b9956a05d824487
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-27 10:29:15 +0530

    added feature to create post

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -1,8 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract SocialNetwork{
+
     string public name;
+    uint public postCount=0;
     constructor() public{
         name="BlockChain Social Network";
     }
+
+    //key value store to write data into blockchain
+    mapping(uint => Post) public posts;
+
+    // struct for the post
+    struct Post{
+        uint id; //unsigned integer
+        string content;
+        uint tipAmount;
+        address author; //hex address
+    }
+
+    event PostCreated(
+        uint id,
+        string content,
+        uint tipAmount,
+        address author
+    );
+
+    // function to create posts
+    function createPost(string memory _content) public {
+        // require valid _content
+        require(bytes(_content).length > 0);  // returns true or false
+        postCount++;
+    // msg is a special global variable
+        posts[postCount]=  Post(postCount,_content,0,msg.sender);   // mapping to the posts map declared above
+        // Events
+        emit PostCreated(postCount,_content,0,msg.sender);
+    }
+
 }
